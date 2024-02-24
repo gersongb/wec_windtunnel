27254cdcf2aae1f199f5b9700cbd934d98d72b78
Gerson Garsed-Brand <gerson@sabe.tech>
2024-02-24 07:02:39 +0100

    update 24-02-24

--- wt_testing_dashboard.py
+++ wt_testing_dashboard.py
@@ -9,8 +9,8 @@ import windtunnel as wt
 wt_name = 'Sauber'
 #wt_name = 'Windshear'
 
-data_folder = r'Z:\homologation\WT_data\SAUBER-LMH-LMDh\Lambo 2024\Processed_Data'
-input_file = r'Z:\homologation\WT_data\SAUBER-LMH-LMDh\Lambo 2024\E9202_c01_SessionData_Extern_v008.xlsm'
+data_folder = r'Z:\homologation\WT_data\SAUBER-LMGT3\BMW_E9227\Processed_Data'
+input_file = r'Z:\homologation\WT_data\SAUBER-LMGT3\BMW_E9227\E9227_c01_SessionData_Extern_v008.xlsm'
 
 input_folder = r'Z:\homologation\WT_data\SAUBER-LMH-LMDh\BMW 2023\E9212_c01_SessionData_Extern_v008.xlsm'
 windshear_meta = r'Z:\homologation\WT_data\WINDSHEAR-LMDh\LAMBO-2023\Windshear_Meta'
@@ -24,14 +24,14 @@ dCz_status = 0.0
 data_file_status = ''
 
 # Homologation window Sauber LMH:
-window_pd = pd.DataFrame({
-    'Cx': [1.040, 1.040, 1.024, 0.98, 1.040],
-    'Cz': [4.048, 4.230, 4.230, 4.048, 4.048]
-})
+#window_pd = pd.DataFrame({
+#    'Cx': [1.040, 1.040, 1.024, 0.98, 1.040],
+#    'Cz': [4.048, 4.230, 4.230, 4.048, 4.048]
+#})
 
 # Braking window Sauber LMH:
-blanking_dx = 0.01
-blanking_dz = 0.035
+#blanking_dx = 0.01
+#blanking_dz = 0.035
 
 print(input_file)
 print(wt_columns)
@@ -45,14 +45,14 @@ print(wt_columns)
 
 
 # Homologation window Sauber LMGT3:
-#window_pd = pd.DataFrame({
-#    'Cx': [1.107, 1.161, 1.308, 1.255, 1.107],
-#    'Cz': [2.850, 3.245, 3.357, 2.962, 2.850]
-#})
+window_pd = pd.DataFrame({
+    'Cx': [1.107, 1.161, 1.308, 1.255, 1.107],
+    'Cz': [2.850, 3.245, 3.357, 2.962, 2.850]
+})
 
 # Braking window Sauber LMGT3:
-#blanking_dx = 0.01
-#blanking_dz = 0.05
+blanking_dx = 0.01
+blanking_dz = 0.05
 
 #  Create a dictionary of colours for the plotly graph:
 colours = {
